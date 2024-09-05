590301f58d52e1e403013d7dbc1020068eed013e
pavel.nemecek <fenrix667@gmail.com>
2024-09-05 10:37:03 +0200

    ....

---
+++ src/src/Opakování/FileBasics.java
@@ -0,0 +1,9 @@
+package Opakování;
+
+import fileworks.DataExport;
+
+public class FileBasics {
+    public static void main(String[] args) {
+        DataExport de = new DataExport("Output.txt");
+    }
+}
