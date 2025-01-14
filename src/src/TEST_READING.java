57d5230d2078b58a500ef42fd00c3c910ecbb653
Fenrix59 <fenrix667@gmail.com>
2025-01-14 18:28:22 +0100

    Dodělání testu z Čtení

--- src/src/TEST_READING.java
+++ src/src/TEST_READING.java
@@ -7,39 +7,57 @@ import java.util.Comparator;
 import java.util.List;
 
 public class TEST_READING {
+    final static Comparator<File> BY_SIZE = new Comparator<File>() {
+
+        @Override
+        public int compare(File o1, File o2) {
+            return Long.compare(o1.length(), o2.length());
+        }
+    };
+
+    static void printSortedFiles(ArrayList<File> files) {
+        files.sort(BY_SIZE);
+        for (File f : files) {
+            System.out.println(f + ": " + f.length());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         String path = "testData";
         File folder = new File("src\\src\\testData");
-        if (folder.isDirectory()){
+        if (folder.isDirectory()) {
             System.out.println("Jedná se o složku");
             File[] files = folder.listFiles();
-            System.out.println(files);
-            ArrayList<String> arrayFile = new ArrayList<>();
-            ArrayList<String> arrayFile2 = new ArrayList<>();
+            ArrayList<File> Filearraylist = new ArrayList<>();
             System.out.println(Arrays.toString(files));
-            for(File file : files){
-                arrayFile.add(file.toString());
-            }
-            for (int i = 0; i <arrayFile.size() ; i++) {
-                if (arrayFile.get(i).contains(".txt")){
-                    arrayFile2.add(arrayFile.get(i));
+            for (File file : files) {
+                if (file.toString().contains(".txt")) {
+                    Filearraylist.add(file);
                 }
             }
-            System.out.println(arrayFile2);
-            ArrayList sss = new ArrayList();
+            printSortedFiles(Filearraylist);
+
             String line;
+            BufferedReader toRead;
 
-
-            for (int i = 0; i < arrayFile2.size() ; i++) {
-               BufferedReader toRead = new BufferedReader(new FileReader(new File(arrayFile2.get(i))));
+            ArrayList<String> sss = new ArrayList<>();
+            for (int i = 0; i < Filearraylist.size(); i++) {
+                toRead = new BufferedReader(new FileReader(new File(String.valueOf(Filearraylist.get(i)))));
                 line = toRead.readLine();
                 System.out.println(line);
                 sss.add(line);
             }
             System.out.println(sss);
-        }
-        else{
-            System.out.println("nejedná se o složku, zadej jinou cestu");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File("basnicka.txt")));
+            for (int i = 0; i < sss.size(); i++) {
+                bw.write(sss.get(i));
+            }
+            bw.close();
+
+
+        } else {
+            System.out.println("Nejedná se o složku");
         }
     }
 }
+
